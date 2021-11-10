561d56d4e03ae43b84825d77a35fba8a96d293ef
dedaje <94041899+dedaje@users.noreply.github.com>
2021-11-10 10:26:08 +0100

    Add files via upload

---
+++ test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!" + (new diller()).Add(1,1));
+
+            Console.ReadKey();
+        }
+    }
+}
